a6f2a83a4267ef58f2a606d59e1c683ef57db947
Gary Fredericks <fredericksgary@gmail.com>
2011-06-24 16:00:00 -0400

    Extract the meta-test helpers into a separate ns

--- test/greengrocer/test/core.clj
+++ test/greengrocer/test/core.clj
@@ -1,33 +1,7 @@
 (ns greengrocer.test.core
   (:use [greengrocer.core] :reload)
+  (:use greengrocer.test.meta-test)
   (:use [clojure.test]))
-
-(defmacro get-test-results
-  [& body]
-  `(binding
-     [*report-counters* (ref *initial-report-counters*),
-      *testing-vars* (list),
-      *testing-contexts* (list),
-      *test-out* (new java.io.FileWriter "/dev/null")]
-     ~@body
-     @*report-counters*))
-
-(defmacro should-fail
-  [& body]
-  `(let [res# (get-test-results ~@body)]
-     (is (pos? (+ (:fail res#) (:error res#))))))
-
-(defmacro should-succeed
-  [& body]
-  `(let [res# (get-test-results ~@body)]
-     (is (zero? (+ (:fail res#) (:error res#))))
-     (is (pos? (:pass res#)))))
-
-(deftest should-fail-test
-  (should-fail (is (zero? 4))))
-
-(deftest should-succeed-test
-  (should-succeed (is (pos? 4))))
 
 (deftest http-success-test
   (should-succeed
