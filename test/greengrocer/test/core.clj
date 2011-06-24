deee779795f4125f218bcd4ccf84cfb9a6718c99
Gary Fredericks <fredericksgary@gmail.com>
2011-06-24 14:53:49 -0400

    First actual test

--- test/greengrocer/test/core.clj
+++ test/greengrocer/test/core.clj
@@ -28,3 +28,17 @@
 
 (deftest should-succeed-test
   (should-succeed (is (pos? 4))))
+
+(deftest http-success-test
+  (should-succeed
+    (should-be-successful
+      {:status 200}))
+  (should-fail
+    (should-be-successful
+      {:status 404}))
+  (should-succeed
+    (should-not-be-successful
+      {:status 404}))
+  (should-fail
+    (should-not-be-successful
+      {:status 300})))
