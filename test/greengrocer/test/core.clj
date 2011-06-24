727ebccaf4624a3b82920af44d9df5d382238413
Gary Fredericks <fredericksgary@gmail.com>
2011-06-24 14:18:34 -0400

    A bit of helper code for meta-testing

--- test/greengrocer/test/core.clj
+++ test/greengrocer/test/core.clj
@@ -2,5 +2,29 @@
   (:use [greengrocer.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defmacro get-test-results
+  [& body]
+  `(binding
+     [*report-counters* (ref *initial-report-counters*),
+      *testing-vars* (list),
+      *testing-contexts* (list),
+      *test-out* (new java.io.FileWriter "/dev/null")]
+     ~@body
+     @*report-counters*))
+
+(defmacro should-fail
+  [& body]
+  `(let [res# (get-test-results ~@body)]
+     (is (pos? (+ (:fail res#) (:error res#))))))
+
+(defmacro should-succeed
+  [& body]
+  `(let [res# (get-test-results ~@body)]
+     (is (zero? (+ (:fail res#) (:error res#))))
+     (is (pos? (:pass res#)))))
+
+(deftest should-fail-test
+  (should-fail (is (zero? 4))))
+
+(deftest should-succeed-test
+  (should-succeed (is (pos? 4))))
