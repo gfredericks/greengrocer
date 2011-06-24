a1dcfebaf88258fbaa79635d3984d8ed787360c9
Gary Fredericks <fredericksgary@gmail.com>
2011-06-24 17:14:54 -0400

    version bump 0.0.3

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject greengrocer "0.0.2"
-  :description "FIXME: write"
+(defproject greengrocer "0.0.3"
+  :description "A syntactically functional and semantically behavioral
+                testing framework for ring web-apps."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [enlive "1.0.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
